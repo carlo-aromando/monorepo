829f5a0a2c451d2c1ef730b1444d3e6e62248772
carlo-aromando <carlo.aromando@cubbit.io>
2025-08-06 12:10:36 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Monorepo test";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          gopls
+          golangci-lint
+          gotools
+          goreleaser
+          bazel_7
+        ];
+        shellHook = ''
+          echo "Welcome to monorepo dev shell"
+        '';
+      };
+    });
+}
